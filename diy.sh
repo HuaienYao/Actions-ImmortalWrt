937d326c2c88e65a142f15ca270ea01464e7c3ba
Wyane <wyane.yia@gmail.com>
2026-04-05 17:27:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,6 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
+git clone --depth=1 https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-openclash/luci-app-openclash
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
